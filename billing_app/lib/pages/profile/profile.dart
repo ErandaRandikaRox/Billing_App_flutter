34f2a99410701ffad42a927b951b78f1d847b195
ErandaRandikaRox <160154662+ErandaRandikaRox@users.noreply.github.com>
2025-03-29 22:46:59 +0530

    update

--- billing_app/lib/pages/profile/profile.dart
+++ billing_app/lib/pages/profile/profile.dart
@@ -1,4 +1,3 @@
-import 'package:billing_app/navigation_bar.dart';
 import 'package:billing_app/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -7,89 +6,95 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        title: "Profile Page",
-        actions: [
-          IconButton(
-            icon: Icon(Icons.edit, color: Colors.black),
-            onPressed: () {
-              // Add edit profile functionality here
-            },
-          ),
-        ],
-        showDrawerIcons: true,
-        showBackButton: false,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.blue.shade50, Colors.white],
+    return SafeArea(
+      child: Scaffold(
+        appBar: CustomAppBar(
+          title: "Profile Page",
+          actions: [
+            IconButton(
+              icon: Icon(Icons.edit, color: Colors.black),
+              onPressed: () {
+                // Add edit profile functionality here
+              },
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: 20),
-                CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.blueAccent,
-                  child: Icon(Icons.person, size: 60, color: Colors.white),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  "Charaka",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blueGrey.shade700,
+          ],
+          showDrawerIcons: true,
+          showBackButton: false,
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.blue.shade50, Colors.white],
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.blueAccent,
+                    child: Icon(Icons.person, size: 60, color: Colors.white),
                   ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  "charakae@example.com",
-                  style: TextStyle(fontSize: 16, color: Colors.grey.shade600),
-                ),
-                SizedBox(height: 20),
-                _buildInfoCard("Phone", "+947773333", Icons.phone),
-                _buildInfoCard(
-                  "Address",
-                  "123 Main St, Matale",
-                  Icons.location_on,
-                ),
-                _buildInfoCard(
-                  "Joined",
-                  "March 25, 2023",
-                  Icons.calendar_today,
-                ),
-                _buildInfoCard("Salary / Month", "1000", Icons.calendar_today),
-                SizedBox(height: 20),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.deepPurple,
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+                  SizedBox(height: 16),
+                  Text(
+                    "Charaka",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey.shade700,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "charakae@example.com",
+                    style: TextStyle(fontSize: 16, color: Colors.grey.shade600),
+                  ),
+                  SizedBox(height: 20),
+                  _buildInfoCard("Phone", "+947773333", Icons.phone),
+                  _buildInfoCard(
+                    "Address",
+                    "123 Main St, Matale",
+                    Icons.location_on,
+                  ),
+                  _buildInfoCard(
+                    "Joined",
+                    "March 25, 2023",
+                    Icons.calendar_today,
+                  ),
+                  _buildInfoCard(
+                    "Salary / Month",
+                    "1000",
+                    Icons.calendar_today,
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurple,
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 5,
                       ),
-                      elevation: 5,
-                    ),
-                    onPressed: () {
-                      // Add logout functionality here
-                    },
-                    child: Text(
-                      "Logout",
-                      style: TextStyle(fontSize: 16, color: Colors.white),
+                      onPressed: () {
+                        // Add logout functionality here
+                      },
+                      child: Text(
+                        "Logout",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
