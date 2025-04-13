8d16b491a7e898f6d2811a3e2d1d23f74e3740fd
ErandaRandikaRox <160154662+ErandaRandikaRox@users.noreply.github.com>
2025-04-13 07:40:38 +0530

    creadit bill paert updated

--- billing_app/lib/pages/credit_bill/credit_bill_page.dart
+++ billing_app/lib/pages/credit_bill/credit_bill_page.dart
@@ -5,6 +5,23 @@ import 'package:flutter/material.dart';
 class CreditBillPage extends StatelessWidget {
   const CreditBillPage({super.key});
 
+  Widget _creditBillCard(String title, String amount) {
+    return Card(
+      color: Colors.grey.shade300,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      elevation: 5,
+      child: ListTile(
+        leading: Icon(Icons.credit_card, color: Colors.deepPurple),
+        title: Text(title),
+        subtitle: Text("Bill amount: $amount"),
+        trailing: Icon(Icons.arrow_forward_ios),
+        onTap: () {
+          // Handle action if needed
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +32,22 @@ class CreditBillPage extends StatelessWidget {
         showBackButton: false,
       ),
       drawer: CustomDrawer(),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            _creditBillCard("Bomba kade", "1000"),
+            SizedBox(height: 10),
+            _creditBillCard("Lakmini Stores", "2000"),
+            SizedBox(height: 10),
+            _creditBillCard("Rathnayake Shop", "1500"),
+            SizedBox(height: 10),
+            _creditBillCard("New City Traders", "3000"),
+            SizedBox(height: 10),
+            _creditBillCard("Kamal Stores", "1800"),
+          ],
+        ),
+      ),
     );
   }
 }
